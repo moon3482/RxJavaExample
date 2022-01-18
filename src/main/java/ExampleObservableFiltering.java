365af0367815c46ed560981fa8672bc3885b2c2b
Charlie_Moon <ckmoon@smartscore.kr>
2022-01-18 18:10:08 +0900

    ObservableFilter Complete

--- src/main/java/ExampleObservableFiltering.java
+++ src/main/java/ExampleObservableFiltering.java
@@ -76,10 +76,26 @@ class ExampleFilter {
 class ExampleFirst {
     public static void main(String[] args) {
         Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).first(1000).subscribe(s -> {
-            System.out.println("데이터 : " + s);
+            System.out.println("데이터1 : " + s);
         });
+        System.out.println("---------------------------------------");
         Observable.empty().first(1000).subscribe(s -> {
-            System.out.println("데이터 : " + s);
+            System.out.println("데이터2 : " + s);
+        });
+    }
+}
+
+/**
+ * last - 마지막 데이터를 발행하는 필터 만약 데이터가 없다면 기본값으로 설정한 데이터를 발행한다.
+ */
+class ExampleLast {
+    public static void main(String[] args) {
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).last(2000).subscribe(s -> {
+            System.out.println("데이터1 : " + s);
+        });
+        System.out.println("---------------------------------------");
+        Observable.empty().last(2000).subscribe(s -> {
+            System.out.println("데이터2 : " + s);
         });
     }
 }
@@ -100,42 +116,84 @@ class ExampleIgnoreElements {
                 emitter.onError(new Throwable("에러났어요~"));
 //                emitter.onComplete();
             }
-        }).ignoreElements().subscribe(()->{System.out.println("컴플리트!");},e->{System.out.println(e);});
+        }).ignoreElements().subscribe(() -> {
+            System.out.println("컴플리트!");
+        }, e -> {
+            System.out.println(e);
+        });
     }
 }
 
-class ExampleLast {
-    public static void main(String[] args) {
-
-    }
-}
-
+/**
+ * sample - 특정 시간간격을 기준으로 가장 최근 데이터를 발행해주는 필터
+ */
 class ExampleSample {
     public static void main(String[] args) {
+        Observable.interval(1000L, TimeUnit.MILLISECONDS).sample(2500L, TimeUnit.MILLISECONDS).subscribe(s -> {
+            System.out.println("데이터 : " + s);
+        });
 
+        while (true) {
+        }
     }
 }
 
+/**
+ * skip - 최초 n개의 데이터를 건너뛰고 발행하는 필터
+ */
 class ExampleSkip {
     public static void main(String[] args) {
-
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).skip(3).subscribe(s -> {
+            System.out.println("데이터1 : " + s);
+        });
+        System.out.println("---------------------------------------");
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).skip(7).subscribe(s -> {
+            System.out.println("데이터2 : " + s);
+        });
     }
 }
 
+/**
+ * skipLast - 마지막 데이터 n개를 건너뛰고 발횅하는 필터
+ */
 class ExampleSkipLast {
     public static void main(String[] args) {
-
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).skipLast(3).subscribe(s -> {
+            System.out.println("데이터1 : " + s);
+        });
+        System.out.println("---------------------------------------");
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).skipLast(7).subscribe(s -> {
+            System.out.println("데이터2 : " + s);
+        });
     }
 }
 
+/**
+ * take - 최초 데이터 n개만 발행하는 필터
+ */
 class ExampleTake {
     public static void main(String[] args) {
-
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).take(3).subscribe(s -> {
+            System.out.println("데이터1 : " + s);
+        });
+        System.out.println("---------------------------------------");
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).take(7).subscribe(s -> {
+            System.out.println("데이터2 : " + s);
+        });
     }
 }
 
+/**
+ * takeLast - 마지막 데이터 n개만 발행하는 필터
+ */
 class ExampleTakeLast {
     public static void main(String[] args) {
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).takeLast(3).subscribe(s -> {
+            System.out.println("데이터1 : " + s);
+        });
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).takeLast(7).subscribe(s -> {
+            System.out.println("데이터2 : " + s);
+        });
 
     }
 }
